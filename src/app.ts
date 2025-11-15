603050a7e25994cfd3e32ee2626e95d587a4f7c1
Krishkumarchhabra25 <krishkumarchhabra90@gmail.com>
2025-11-15 13:12:02 +0530

    added the welcome text

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from "cors"
 import routes from "./routes";
 
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req:Request, res:Response) => {
+  res.send('Welcome to DevOps Server');
+});
 
 app.use("/api/v1/", routes);
 
